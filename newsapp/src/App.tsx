3de78af986f0dce9a87ffc7b830c97b9b1f493ef
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-02-27 23:46:28 +0300

    Added news card component.

--- newsapp/src/App.tsx
+++ newsapp/src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, FlatList, StyleSheet} from 'react-native';
+import news_data from './news_data.json';
+import NewsCard from './components/NewsCard';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View>
-        <Text>News App</Text>
+        <FlatList
+          data={news_data}
+          renderItem={({item}) => <NewsCard news={item} />}
+        />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eceff1',
+  },
+});
 
 export default App;
